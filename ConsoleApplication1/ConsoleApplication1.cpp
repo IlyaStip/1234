7f17c7215a7df9a6af57af37a862b843d0db4493
nikolaykargin <kolya.kargin@gmail.com>
2016-09-12 10:45:57 +0300

    commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,20 @@
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+class CppStudio // им€ класса
+{
+public: // спецификатор доступа
+    void message() // функци€ (метод класса) вывод€ща€ сообщение на экран
+    {
+        cout << "Test /n";
+    }
+}; // конец объ€влени€ класса CppStudio
+ 
+int main(int argc, char* argv[])
+{
+    CppStudio objMessage; // объ€вление объекта
+    objMessage.message(); // вызов функции класса message
+    system("pause");
+    return 0;
+}
